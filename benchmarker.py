cc728d9885196d4cdfc0c255058e34ebb5aefbe1
LeoQ <leoqys@gmail.com>
2018-10-12 21:53:13 +0800

    Code update with DS 5
    code updated and created dataset 5

--- benchmarker.py
+++ benchmarker.py
@@ -9,11 +9,46 @@ import imp
 
 testset_amount = 10
 selected_Dataset = None
+trainingSeq = 0
+testSeq = 0
 test_Question = []
 test_Dir = []
+bm = None
+MajorDataset = "Dataset 5"
 
+def fetchTrainingData(DS_DIR, TRAIN_DIR = "Training Image"):
+    if DS_DIR == MajorDataset and TRAIN_DIR == "Training Image":
+        global trainingSeq
+        if trainingSeq == 0:
+            trainingSeq = 1
+            return fetchTrain(DS_DIR, "Training-Pure")
 
-def fetchTestQuestion():
+        elif trainingSeq == 1:
+            trainingSeq = 2
+            return fetchTrain(DS_DIR, "Training-Mix")
+
+    else:
+        return fetchTrain(DS_DIR, TRAIN_DIR)
+
+def fetchTestQuestion(test_Name = "Test Image"):
+    if selected_Dataset == MajorDataset and test_Name == "Test Image":
+        global testSeq
+        if testSeq == 0:
+            testArr = fetchTest("Test 1")
+            testSeq = 1
+            return testArr
+
+        elif testSeq == 1:
+            testSeq = 2
+            return fetchTest("Test 2 - Angle")
+
+        elif testSeq == 2:
+            testSeq = 3
+            return fetchTest("Test 3 - Lighting")
+    else:
+        return fetchTest(test_Name)
+
+def fetchTest(test_Name = "Test Image"):
     global test_Question
     global test_Dir
 
@@ -27,7 +62,7 @@ def fetchTestQuestion():
         print("** Fetching Test Images... **")
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         img_dir = os.path.join(BASE_DIR, DS_DIR)
-        testset_data = os.path.join(img_dir, "Test Image")
+        testset_data = os.path.join(img_dir, test_Name)
 
         total_files = len(os.listdir(testset_data)) * testset_amount
         rand = random.sample(range(0, total_files), total_files)
@@ -86,15 +121,16 @@ def submitAnswer(ansArr):
     print("No of wrong: " + str(wrongAns))
     acc = (correctAns / (correctAns + wrongAns)) * 100
     print("Accuracy is " + str(acc) + "%\n")
+    return correctAns, wrongAns, acc
 
-def feedTestData(DS_DIR):
+def feedTestData(DS_DIR, TEST_DIR="Test Image"):
     print("**Initiating Test, calling testAlgo() method **")
     try:
         correctAns = 0
         wrongAns = 0
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         img_dir = os.path.join(BASE_DIR, DS_DIR)
-        testset_data = os.path.join(img_dir, "Test Image")
+        testset_data = os.path.join(img_dir, TEST_DIR)
         wrong_dir = os.path.join(BASE_DIR,"Incorrect Answer")
 
         if not os.path.exists(wrong_dir):
@@ -132,12 +168,16 @@ def feedTestData(DS_DIR):
         print("No of wrong: " + str(wrongAns))
         acc = (correctAns / (correctAns+wrongAns))*100
         print("Accuracy is " + str(acc) + "%\n")
+
+        return correctAns, wrongAns, acc
+
     except Exception as e:
         print("Please ensure you code have a method name testAlgo(image)")
         print (e)
 
 
-def fetchTrainingData(DS_DIR):
+
+def fetchTrain(DS_DIR, TRAIN_DIR = "Training Image"):
     try:
         imageArr = []
         labelArr = []
@@ -146,7 +186,7 @@ def fetchTrainingData(DS_DIR):
 
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         img_dir = os.path.join(BASE_DIR, DS_DIR)
-        training_data = os.path.join(img_dir, "Training Image")
+        training_data = os.path.join(img_dir, TRAIN_DIR)
 
         print("Preparing images for training...")
 
@@ -172,6 +212,7 @@ def fetchTrainingData(DS_DIR):
 def main():
     pythonFile = input("Key in your ALGORITHM file name (without .py): ")
     try:
+        global bm
         bm = importlib.import_module(pythonFile, ".")
         menu = True
         spam_info = imp.find_module(pythonFile)
@@ -185,6 +226,7 @@ def main():
             print("2) Dataset 2")
             print("3) Dataset 3")
             print("4) Dataset 4")
+            print("5) Dataset 5")
             print("0) Exit")
 
             try:
@@ -213,6 +255,41 @@ def main():
                     imageArr, labelArr, DS_DIR = fetchTrainingData("Dataset 4")
                     bm.trainAlgo(imageArr, labelArr, DS_DIR)
                     feedTestData("Dataset 4")
+                elif choice == 5:
+                    print("**Initiating training(pure face images), calling trainAlgo() method.**")
+                    print("")
+
+                    imageArr, labelArr, DS_DIR = fetchTrainingData(MajorDataset, "Training-Pure")
+                    bm.trainAlgo(imageArr, labelArr, DS_DIR)
+                    print("Test 1 Started:")
+                    correctAns, wrongAns, acc = feedTestData("Dataset Mix","Test 1")
+
+                    print("\n Initiating training 2(mix factor images), calling trainAlgo() method")
+                    imageArr2, labelArr2, DS_DIR2 = fetchTrainingData(MajorDataset, "Training-Mix")
+                    bm.trainAlgo(imageArr2, labelArr2, DS_DIR2 + "2")
+
+                    print("Test 2 Started: ")
+                    correctAns2, wrongAns2, acc2 = feedTestData(MajorDataset, "Test 2 - Angle")
+                    print("Test 3 Started: ")
+                    correctAns3, wrongAns3, acc3 = feedTestData(MajorDataset, "Test 3 - Lighting")
+
+                    print("================= Test 1 (Pure Faces) Results ========================")
+                    print ("No of correct answer: " + str(correctAns))
+                    print ("No of wrong answer: " + str(wrongAns))
+                    print ("Accuracy: " + str(acc))
+
+                    print("")
+                    print("============= Test 2 (Faces of different angle) Results ==============")
+                    print ("No of correct answer: " + str(correctAns2))
+                    print ("No of wrong answer: " + str(wrongAns2))
+                    print ("Accuracy: " + str(acc2))
+
+                    print("")
+                    print("============= Test 3 (Faces of different lighting) Results ==============")
+                    print ("No of correct answer: " + str(correctAns3))
+                    print ("No of wrong answer: " + str(wrongAns3))
+                    print ("Accuracy: " + str(acc3))
+
                 elif choice == 0:
                     print("Exiting ...")
                     break
